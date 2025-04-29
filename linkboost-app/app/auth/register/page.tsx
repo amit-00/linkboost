6bd6a576ee86be0408f18c1d99f0c2ff1230689a
Amit Verma <amit.v@hotmail.com>
2025-04-28 20:39:41 -0400

    fix bug preventing formData from being captured

--- linkboost-app/app/auth/register/page.tsx
+++ linkboost-app/app/auth/register/page.tsx
@@ -24,7 +24,7 @@ function Register() {
                 <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
               </g>
             </svg>
-            <input type="email" placeholder="mail@site.com" required/>
+            <input name="email" type="email" placeholder="mail@site.com" required/>
           </label>
           <div className="validator-hint hidden">Enter valid email address</div>
 
@@ -45,6 +45,7 @@ function Register() {
               </g>
             </svg>
             <input
+              name="password"
               type="password"
               required
               placeholder="Password"
